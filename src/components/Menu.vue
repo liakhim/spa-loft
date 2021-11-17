809973c7a64f1a670dc941eed4468bd112dad63f
liakhim <mmingareev@bk.ru>
2021-11-18 00:40:00 +0300

    add router, menu html

---
+++ src/components/Menu.vue
@@ -0,0 +1,14 @@
+<template>
+    <ul class="nav">
+        <li class="nav-item">
+            <router-link to="#">
+                Горячие напитки
+            </router-link>
+        </li>
+        <li class="nav-item">
+            <router-link to="#">
+                Холодные напитки
+            </router-link>
+        </li>
+    </ul>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <p>dcbbkbcsajka</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
